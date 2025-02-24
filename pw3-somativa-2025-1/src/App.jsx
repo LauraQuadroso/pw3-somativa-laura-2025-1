1b922286a60138cc5f3b4669aac5c1075adc8fab
LauraQuadroso <lauraqoliveira10@gmailcom>
2025-02-23 22:48:58 -0300

    Edições CSS

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,9 +1,8 @@
 
 import './App.css'
 
-import BookCard from './Components/BookCard'
+import NavBar from './Components/layout/NavBar'
 
-import capa_livro from './assets/cavernas_aco.jpg'
 
 function App() {
  
@@ -11,27 +10,9 @@ function App() {
   return (
     <>
       <div>
-       <h1>Pw3-webapp-livraria </h1>
-
-      <BookCard 
-       titulo='As Cavernas de aço'
-       autor='Izac Azimov'
-       imagem= {capa_livro}
-       />
-
-       {/* <BookCard 
-       titulo= 'o Sol desvelado' 
-       autor='Arthur C.Clarck'
-      imagem= 'vai aparer aqui'
-       />
-
-       <BookCard
-      titulo='Neuromancer' 
-      autor= 'Willian Gibson'
-      imagem= 'vai aparer aqui'
-       />  */}
+       
+        <NavBar />
       
-
       </div>
       
     </>

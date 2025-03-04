dee116d9d910f4f1023a6707695fb83290b5477e
LauraQuadroso <lauraqoliveira10@gmailcom>
2025-03-03 22:02:31 -0300

    telas de navegação

---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './Home.module.css';
+
+const Home = ()=>{
+    return(
+
+        <section className={styles.home_container}>
+
+            <h1>Bem vindo ao WEB APP<span>LIBRI</span></h1>
+            <p>Comece a gerenciar os seus livros agora mesmo!</p>
+            <img  src="./Book_home.png" alt="" />
+
+        </section>
+
+    );
+}
+
+export default Home;
